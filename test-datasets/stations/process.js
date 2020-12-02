af82d62c617854ba885f6392b81c75ac2005109b
Hwan-seok <tttkhs@naver.com>
2020-12-03 03:06:27 +0900

    remove zero from each line number

--- test-datasets/stations/process.js
+++ test-datasets/stations/process.js
@@ -1,13 +1,16 @@
 const path = require("path");
 const fs = require("fs");
 
-const originData = fs.readFileSync(path.resolve(__dirname, "./origin.json"));
+const originData = fs.readFileSync(path.resolve(__dirname, "origin.json"));
 
 const precessedData = JSON.parse(originData)
-  .DATA.map((e) => ({ line_num: e.line_num, station_name: e.station_nm }))
+  .DATA.map((e) => ({
+    line_num: e.line_num.replace(/0([0-9])호선/, "$1호선"),
+    station_name: e.station_nm,
+  }))
   .reduce(
     (prev, curr) => prev + `{"index":{}}\n` + JSON.stringify(curr) + "\n",
     ""
   );
 
-fs.writeFileSync(path.resolve(__dirname, "./processed.json"), precessedData);
+fs.writeFileSync(path.resolve(__dirname, "processed.json"), precessedData);
